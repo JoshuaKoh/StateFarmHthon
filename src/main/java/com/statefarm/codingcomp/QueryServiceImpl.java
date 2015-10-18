2ee4918f5380fa71c4f6ba374d1475add14ff298
Joshua Koh <Joshua@kohfamily.com>
2015-10-17 22:27:18 -0400

    Fixed convertTo methods

--- src/main/java/com/statefarm/codingcomp/QueryServiceImpl.java
+++ src/main/java/com/statefarm/codingcomp/QueryServiceImpl.java
@@ -3,6 +3,7 @@ package com.statefarm.codingcomp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -17,10 +18,11 @@ public class QueryServiceImpl implements QueryService {
 
 	SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
     Reader reader = new Reader();
+    List<User> users = convertToUsers();
+    List<Email> emails = convertToEmails();
 
     @Override
     public List<User> usersByDomain( String domain ) throws Exception {
-		List<User> users = covertToUsers(reader.read( 1, "users.txt"));
         List<User> matchingUsers = new ArrayList<User>();
 
         for (User u: users) {
@@ -29,14 +31,20 @@ public class QueryServiceImpl implements QueryService {
         		matchingUsers.add(u);
         	}
         }
-
         return matchingUsers;
     }
 
     @Override
     public List<Email> emailsInDateRange( Date start, Date end ) throws Exception {
-        // TODO Auto-generated method stub
-        return null;
+        List<Email> returning = new ArrayList<Email>();
+        
+        for (Email e : emails) {
+        	if (e.getSent().after(start) && e.getSent().before(end)) {
+        		returning.add(e);
+        	}
+        }
+        
+        return returning;
     }
 
     @Override
@@ -159,37 +167,66 @@ public class QueryServiceImpl implements QueryService {
         return null;
     }
 
-    private List<User> convertToUsers(String[] userStr) {
-    	List<User> users = new ArrayList<User>();
-    	Scanner sc;
-    	for (String s : userStr) {
-    		sc = new Scanner(s);
-    		sc.useDelimiter(",");
-    		sc.next();
-    		String name = sc.next();
-    		String email = sc.next();
-    		User newUser = new User(name, email);
-    		users.add(newUser);
-    	}
-		return users;
+    private List<User> convertToUsers() {
+		try {
+			String[] userStr1 = reader.read( 1, "users.txt");
+			String[] userStr2 = reader.read( 2, "users.txt");
+			String[] userStr3 = reader.read( 3, "users.txt");
+			String[] userStr4 = reader.read( 4, "users.txt");
+			List<String> rawUsers = new ArrayList<String>();
+			rawUsers.addAll(Arrays.asList(userStr1));
+			rawUsers.addAll(Arrays.asList(userStr2));
+			rawUsers.addAll(Arrays.asList(userStr3));
+			rawUsers.addAll(Arrays.asList(userStr4));
+			List<User> users = new ArrayList<User>();
+			Scanner sc;
+			for (String s : rawUsers) {
+				sc = new Scanner(s);
+				sc.useDelimiter(",");
+				sc.next();
+				String name = sc.next();
+				String email = sc.next();
+				User newUser = new User(name, email);
+				users.add(newUser);
+			}
+			return users;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
     }
 
     // TODO Terrible implementation
-    private List<Email> convertToEmails(String[] emailStr) throws ParseException {
-    	List<Email> emails = new ArrayList<Email>();
-    	Scanner sc;
-    	for (String s : emailStr) {
-    		sc = new Scanner(s);
-    		sc.useDelimiter(",");
-    		String from = sc.next();
-    		String to = sc.next();
-    		String dateStr = sc.next();
-    		dateStr = dateStr.substring(0, 10) + " " + dateStr.substring(12, 19);
-    		Date date = sdf.parse(dateStr);
-    		Email newEmail = new Email(from, to, date);
-    		emails.add(newEmail);
+    private List<Email> convertToEmails() {
+    	try {
+    		
+    		String[] emailStr1 = reader.read( 1, "mail.txt");
+    		String[] emailStr2 = reader.read( 2, "mail.txt");
+    		String[] emailStr3 = reader.read( 3, "mail.txt");
+    		String[] emailStr4 = reader.read( 4, "mail.txt");
+    		List<String> rawEmails = new ArrayList<String>();
+    		rawEmails.addAll(Arrays.asList(emailStr1));
+    		rawEmails.addAll(Arrays.asList(emailStr2));
+    		rawEmails.addAll(Arrays.asList(emailStr3));
+    		rawEmails.addAll(Arrays.asList(emailStr4));
+    		List<Email> emails = new ArrayList<Email>();
+    		Scanner sc;
+    		for (String s : rawEmails) {
+    			sc = new Scanner(s);
+    			sc.useDelimiter(",");
+    			String from = sc.next();
+    			String to = sc.next();
+    			String dateStr = sc.next();
+    			dateStr = dateStr.substring(0, 10) + " " + dateStr.substring(11, 19);
+    			Date date = sdf.parse(dateStr);
+    			Email newEmail = new Email(from, to, date);
+    			emails.add(newEmail);
+    		}
+    		return emails;
+    	} catch (Exception e) {
+    		e.printStackTrace();
     	}
-    	return emails;
+    	return null;
     }
 
 }
